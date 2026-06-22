5fc20455765d12963e43e710efc8d4422a22b318
Vitória Zanchet <vitoria.zanchet@hotmail.com>
2026-06-22 14:34:20 -0300

    feat: removendo tag <span> da página Home.jsx

--- cafeteria/src/pages/Home/Home.jsx
+++ cafeteria/src/pages/Home/Home.jsx
@@ -108,9 +108,7 @@ export default function Home() {
                 </ul>
               </div>
             )}
-            <span className={styles.cliqueAviso}>
-              {cafeEstendido ? '' : ''}
-            </span>
+            
           </div>
 
           <div 
